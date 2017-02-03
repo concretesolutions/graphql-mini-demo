cbe2721ffde10e35dfa301de9530a09c8d9946c4
AlexisNo <alexis@henaut.net>
2017-02-03 17:22:24 -0200

    Install GraphQL

---
+++ schema.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+
+module.exports = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
+    }
+  })
+});
